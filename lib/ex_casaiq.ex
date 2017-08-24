fd0611100c811b3073092098f0a9893899567c5d
techgaun <coolsamar207@gmail.com>
2017-08-24 16:28:39 -0500

    initial commit for casaiq sdk for elixir

---
+++ lib/ex_casaiq.ex
@@ -0,0 +1,18 @@
+defmodule CasaIQ do
+  @moduledoc """
+  Documentation for CasaIQ.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CasaIQ.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_casaiq_test.exs
@@ -0,0 +1,8 @@
+defmodule CasaIQTest do
+  use ExUnit.Case
+  doctest CasaIQ
+
+  test "greets the world" do
+    assert CasaIQ.hello() == :world
+  end
+end
